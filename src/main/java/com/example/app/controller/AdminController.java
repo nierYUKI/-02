610b5ea1bc2e303bb1c39194c2f21b70a1c76e3d
miyoshi <miyoshi@sample.com>
2024-02-06 15:23:28 +0900

    管理者アカウントでログインしたら、Formで日付を送信したら、その日の希望シフトを一覧で出せた。

--- src/main/java/com/example/app/controller/AdminController.java
+++ src/main/java/com/example/app/controller/AdminController.java
@@ -1,9 +1,14 @@
 package com.example.app.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.app.domain.ShiftPreferences;
 import com.example.app.domain.Users;
@@ -30,8 +35,17 @@ public class AdminController {
 	session.getAttribute("loggedInUser");
 	model.addAttribute("shiftPreferences",new ShiftPreferences());
 	return "adminHome";
-	
-	
-	
 	}
+	
+	@PostMapping("/adminHome")//アルバイトの希望シフト一覧
+	public String postAdminShiftList(Model model,ShiftPreferences shiftPreferences,
+			@RequestParam LocalDate selectDate
+			) {
+		List<ShiftPreferences> UsersShiftPreferencesList = shiftPreferencesMapper.selectShiftByDate(selectDate);
+		System.out.println(UsersShiftPreferencesList);
+		model.addAttribute("ShiftPreferences",UsersShiftPreferencesList);
+		return "adminHome";
+	}
+	
+	
 }
--- src/main/java/com/example/app/domain/ShiftPreferences.java
+++ src/main/java/com/example/app/domain/ShiftPreferences.java
@@ -12,6 +12,7 @@ import lombok.Data;
 public class ShiftPreferences {
 	private Integer preferenceId;//主キー
 	private Integer userId;//外部キー、ユーザーテーブルと関連付け
+	
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private LocalDate dayOfWeek;//希望シフトの曜日
 	@DateTimeFormat(pattern="HH:mm")
@@ -20,4 +21,9 @@ public class ShiftPreferences {
 	private LocalTime endTime;//希望シフトの終了時間
 	private Integer isApproved;//希望シフトが承認されたかのフラグ
 
+	//2月6日管理者のページにアルバイトの名前を表示する為に追記
+	//ワークベンチではユーザーの名前を取得しているけど、JavaのDomain(箱)に
+	//その記述が必要
+	private String username;
+	private String rankName;
 }
