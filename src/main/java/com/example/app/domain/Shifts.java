7c2cd2875f8853a67ead8f9eaaf2ad176ddf5635
miyoshi <miyoshi@sample.com>
2024-02-08 10:20:28 +0900

    domainのShiftsに確定シフト日を追記

--- src/main/java/com/example/app/domain/Shifts.java
+++ src/main/java/com/example/app/domain/Shifts.java
@@ -2,6 +2,8 @@ package com.example.app.domain;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
@@ -10,10 +12,16 @@ public class Shifts {
 	
 	private Integer shiftId;//主キー
 	private Integer userId;//外部キー、ユーザーテーブルと関連付け
+	@DateTimeFormat(pattern="HH:mm")
 	private LocalDate startDateTime;//シフトの開始時間
+	@DateTimeFormat(pattern="HH:mm")
 	private LocalDate endDateTime;//シフトの終了時間
 	private Integer isApproved;//シフトが承認されたかどうかのフラグ
 	private Integer shiftType;//早番、遅番、夜勤などのタイプ分け
 	private Integer breakDuration;//シフトの休憩時間
+	
+	//2月8日に追記。シフト日を入れるの忘れていた；；
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private LocalDate dayOfWeek;//確定シフト日
 
 }
